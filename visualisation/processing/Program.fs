0f66adeb7ecd8e2bf8d47c299d8ecc164d9c7a2c
AndrewIOM <AndrewIOM@users.noreply.github.com>
2023-09-18 14:04:19 +0100

    Add all sites and proxy groups to map

--- visualisation/processing/Program.fs
+++ visualisation/processing/Program.fs
@@ -4,8 +4,8 @@ open BiodiversityCoder.Core.GraphStructure
 open FSharp.Data
 
 type IndividualMeasureCsv = CsvProvider<
-    Sample = "source_id, source_title, source_year, site_name, LatDD, LonDD, inferred_from, inferred_using, biodiversity_measure, inferred_as, sample_origin, earliest_extent, latest_extent",
-    Schema = "source_id (string option), source_title (string option), source_year (int option), site_name (string), LatDD (float), LonDD (float), inferred_from (string option), inferred_using (string option), biodiversity_measure (string option), inferred_as (string), sample_origin (string), earliest_extent (int option), latest_extent (int option)", HasHeaders = true>
+    Sample = "source_id, source_title, source_year, site_name, LatDD, LonDD, inferred_from, inferred_using, biodiversity_measure, inferred_as, sample_origin, earliest_extent, latest_extent, proxy_category",
+    Schema = "source_id (string option), source_title (string option), source_year (int option), site_name (string), LatDD (float), LonDD (float), inferred_from (string option), inferred_using (string option), biodiversity_measure (string option), inferred_as (string), sample_origin (string), earliest_extent (int option), latest_extent (int option), proxy_category (string)", HasHeaders = true>
 
 let unwrap (f:float<_>) = float f
 
@@ -16,6 +16,15 @@ let origin = function
     | Population.Context.Subfossil -> "Subfossil material"
     | Population.Context.LivingOrganism -> "Living organism"
     | Population.Context.OtherOrigin (a,_) -> "Other"
+
+let proxyToGroup = function
+    | Population.BioticProxies.ContemporaneousWholeOrganism _ -> "Contemporaneous Whole Organism"
+    | Population.BioticProxies.AncientDNA _ -> "Ancient DNA"
+    | Population.BioticProxies.Morphotype m ->
+        match m with
+        | Population.BioticProxies.Macrofossil _-> "Marcofossil"
+        | Population.BioticProxies.Megafossil _ -> "Megafossil"
+        | Population.BioticProxies.Microfossil (g,_) -> g.ToString()
 
 let run () = 
     result {
@@ -156,6 +165,23 @@ let run () =
                                     | _ -> false) |> Option.map (fst >> Storage.loadAtom graph.Directory (typeof<Population.BioticProxies.BioticProxyNode>.Name))
                                     |> Result.ofOption "Could not load inferred from node" |> Result.bind id
                             
+                            let proxyGroups =
+                                popnRelations |> List.where(fun r ->
+                                    match snd r with
+                                    | Population.PopulationRelation.InferredFrom -> true
+                                    | _ -> false) 
+                                |> List.map (fst >> Storage.loadAtom graph.Directory (typeof<Population.BioticProxies.BioticProxyNode>.Name))
+                                |> Result.ofList
+                                |> Result.bind( fun p ->
+                                    p |> List.map(fun p ->
+                                        match p |> fst |> snd with
+                                        | GraphStructure.PopulationNode p ->
+                                            match p with
+                                            | PopulationNode.BioticProxyNode p -> Ok <| proxyToGroup p
+                                            | _ -> Error "not correct node"
+                                        | _ -> Error "not correct node" )
+                                    |> Result.ofList )
+
                             let inferredUsing =
                                 popnRelations |> List.tryFind(fun r ->
                                     match snd r with
@@ -181,58 +207,122 @@ let run () =
                             let using = inferredUsing |> Result.lift(fun (n: Graph.Atom<GraphStructure.Node,obj>) -> (n |> fst |> snd).DisplayName()) |> Result.toOption
                             let by = outcomeMeasure |> Result.lift(fun (n: Graph.Atom<GraphStructure.Node,obj>) -> (n |> fst |> snd).DisplayName()) |> Result.toOption
                             let asT = inferredAs |> Result.lift(fun n -> n |> List.map(fun (n: Graph.Atom<GraphStructure.Node,obj>) -> (n |> fst |> snd).DisplayName()))
-                            asT |> Result.lift(fun taxa -> (from, using, by, (taxa |> String.concat ";")))
+                            asT |> Result.lift(fun taxa -> (from, using, by, (taxa |> String.concat ";"), proxyGroups))
                         ) |> Result.ofList
                     )
 
-                spatialRelation
-                |> Result.bind(fun (_,sinkId,_,_) -> Storage.loadAtom graph.Directory (typeof<Population.Context.ContextNode>.Name) sinkId)
-                |> Result.bind(fun contextNode ->
-                    match contextNode |> fst |> snd with
-                    | GraphStructure.Node.PopulationNode p ->
-                        match p with
-                        | PopulationNode.ContextNode c -> Ok c
-                        | _ -> Error "not a context node"
-                    | _ -> Error "not a context node" )
-                |> Result.bind(fun context ->
-                    match context.SamplingLocation with
-                    | FieldDataTypes.Geography.Site (lat: FieldDataTypes.Geography.Latitude,lon) -> 
-                        // Multiply out for each biodiversity outcome record.
-                        biodiversityOutcomes
-                        |> Result.lift(fun ls ->
-                            ls |> List.map(fun (from, using, by, taxon) ->
-                                sId, sourceName, year, context.Name.Value, unwrap lat.Value, unwrap lon.Value, from, using, by, taxon, origin context.SampleOrigin, earliestExtent, latestExtent))
-                    | FieldDataTypes.Geography.Area poly ->
-                        let lat = poly.Value |> List.map fst |> List.averageBy(fun v -> v.Value |> unwrap)
-                        let lon = poly.Value |> List.map snd |> List.averageBy(fun v -> v.Value |> unwrap)
-                        biodiversityOutcomes
-                        |> Result.lift(fun ls ->
-                            ls |> List.map(fun (from, using, by, taxon) ->
-                                sId, sourceName, year, context.Name.Value, lat, lon, from, using, by, taxon, origin context.SampleOrigin , earliestExtent, latestExtent))
-                    | _ -> Ok [])
-                |> Result.lift(fun l -> l)
-            ) |> List.choose Result.toOption |> List.concat
+                // Load one to many proxy groups
+                let proxyGroups =
+                    extent |> snd |> List.choose(fun (_,sinkId,_,r) ->
+                        match r with
+                        | GraphStructure.Relation.Exposure r ->
+                            match r with
+                            | Exposure.ExposureRelation.HasProxyCategory -> Some sinkId
+                            | _ -> None
+                        | _ -> None)
+                    |> Storage.loadAtoms graph.Directory (typeof<Population.BioticProxies.BioticProxyCategoryNode>.Name)
+                    |> Result.lift(fun l ->
+                        l |> List.where(fun l2 ->
+                            match l2 |> fst |> snd with
+                            | GraphStructure.Node.PopulationNode p ->
+                                match p with
+                                | PopulationNode.BioticProxyCategoryNode _ -> true
+                                | _ -> false
+                            | _ -> false)
+                        |> List.map(fun (category: Graph.Atom<Node,obj>) -> (category |> fst |> snd).DisplayName()))
 
-        // Make a row for each biodiversity outcome
-        printfn "Generated %i row(s)" withSpatialContextAndOutcomes.Length
-        let csv = new IndividualMeasureCsv (withSpatialContextAndOutcomes |> List.map IndividualMeasureCsv.Row)
+                let allProxyGroups =
+                    biodiversityOutcomes
+                    |> Result.bind(fun l -> l |> List.map(fun (_,_,_,_,g) -> g) |> Result.ofList)
+                    |> Result.lift(fun l -> match proxyGroups with | Ok p -> List.concat [l |> List.concat; p] | Error _ -> l |> List.concat)
+                    |> Result.lift(fun l -> l |> List.distinct |> String.concat ";")
+
+                // Add proxy groups from biotic 
+
+                let individualRecords =
+                    spatialRelation
+                    |> Result.bind(fun (_,sinkId,_,_) -> Storage.loadAtom graph.Directory (typeof<Population.Context.ContextNode>.Name) sinkId)
+                    |> Result.bind(fun contextNode ->
+                        match contextNode |> fst |> snd with
+                        | GraphStructure.Node.PopulationNode p ->
+                            match p with
+                            | PopulationNode.ContextNode c -> Ok c
+                            | _ -> Error "not a context node"
+                        | _ -> Error "not a context node" )
+                    |> Result.bind(fun context ->
+                        match context.SamplingLocation with
+                        | FieldDataTypes.Geography.Site (lat: FieldDataTypes.Geography.Latitude,lon) -> 
+                            // Multiply out for each biodiversity outcome record.
+                            biodiversityOutcomes
+                            |> Result.lift(fun ls ->
+                                ls |> List.map(fun (from, using, by, taxon, groups) ->
+                                    sId, sourceName, year, context.Name.Value, unwrap lat.Value, unwrap lon.Value, from, using, by, taxon, origin context.SampleOrigin, earliestExtent, latestExtent, groups |> forceOk |> String.concat ";"))
+                        | FieldDataTypes.Geography.Area poly ->
+                            let lat = poly.Value |> List.map fst |> List.averageBy(fun v -> v.Value |> unwrap)
+                            let lon = poly.Value |> List.map snd |> List.averageBy(fun v -> v.Value |> unwrap)
+                            biodiversityOutcomes
+                            |> Result.lift(fun ls ->
+                                ls |> List.map(fun (from, using, by, taxon, groups) ->
+                                    sId, sourceName, year, context.Name.Value, lat, lon, from, using, by, taxon, origin context.SampleOrigin , earliestExtent, latestExtent, groups |> forceOk |> String.concat ";"))
+                        | _ -> Ok [])
+
+                let includedSites =
+                    spatialRelation
+                    |> Result.bind(fun (_,sinkId,_,_) -> Storage.loadAtom graph.Directory (typeof<Population.Context.ContextNode>.Name) sinkId)
+                    |> Result.bind(fun contextNode ->
+                        match contextNode |> fst |> snd with
+                        | GraphStructure.Node.PopulationNode p ->
+                            match p with
+                            | PopulationNode.ContextNode c -> Ok c
+                            | _ -> Error "not a context node"
+                        | _ -> Error "not a context node" )
+                    |> Result.bind(fun context ->
+                        match context.SamplingLocation with
+                        | FieldDataTypes.Geography.Site (lat: FieldDataTypes.Geography.Latitude,lon) -> 
+                            // Multiply out for each biodiversity outcome record.
+                            biodiversityOutcomes
+                            |> Result.lift(fun ls ->
+                                let from = ls |> List.choose(fun (from, using, by, taxa, groups) -> from) |> List.distinct |> String.concat ";"
+                                let using = ls |> List.choose(fun (from, using, by, taxa, groups) -> using) |> List.distinct |> String.concat ";"
+                                let by = ls |> List.choose(fun (from, using, by, taxa, groups) -> by) |> List.distinct |> String.concat ";"
+                                let taxa = ls |> List.map(fun (from, using, by, taxa, groups) -> taxa) |> List.distinct |> String.concat ";"
+                                [ sId, sourceName, year, context.Name.Value, unwrap lat.Value, unwrap lon.Value, Some from, Some using, Some by, taxa, origin context.SampleOrigin , earliestExtent, latestExtent, allProxyGroups |> forceOk ]
+                                )
+                        | FieldDataTypes.Geography.Area poly -> 
+                            let lat = poly.Value |> List.map fst |> List.averageBy(fun v -> v.Value |> unwrap)
+                            let lon = poly.Value |> List.map snd |> List.averageBy(fun v -> v.Value |> unwrap)
+                            // Multiply out for each biodiversity outcome record.
+                            biodiversityOutcomes
+                            |> Result.lift(fun ls ->
+                                let from = ls |> List.choose(fun (from, using, by, taxa, groups) -> from) |> List.distinct |> String.concat ";"
+                                let using = ls |> List.choose(fun (from, using, by, taxa, groups) -> using) |> List.distinct |> String.concat ";"
+                                let by = ls |> List.choose(fun (from, using, by, taxa, groups) -> by) |> List.distinct |> String.concat ";"
+                                let taxa = ls |> List.map(fun (from, using, by, taxa, groups) -> taxa) |> List.distinct |> String.concat ";"
+                                [ sId, sourceName, year, context.Name.Value, lat, lon, Some from, Some using, Some by, taxa, origin context.SampleOrigin , earliestExtent, latestExtent, allProxyGroups |> forceOk ]
+                                )
+                        | _ -> Ok [])
+
+                (match individualRecords with | Ok i -> i | Error _ -> []),
+                (match includedSites with | Ok i -> i | Error e -> printfn "Error was %s" e; [])
+            )
+
+        // 1. Save individual records
+        let allRecords = 
+            withSpatialContextAndOutcomes 
+            |> List.map fst
+            |> List.concat
+        printfn "Generated %i row(s)" allRecords.Length
+        let csv = new IndividualMeasureCsv (allRecords |> List.map IndividualMeasureCsv.Row)
         let csvStr = csv.SaveToString('\t')
         System.IO.File.WriteAllText("../thalloo-static-site/map-data/ahbdb.txt", csvStr)
 
-        // Make a new row for each site
-        let asSiteGroups =
-            withSpatialContextAndOutcomes
-            |> Seq.groupBy(fun (a,_,_,d,_,_,_,_,_,_,_,_,_) -> a,d)
-            |> Seq.map(fun (_,rows) ->
-                let (a,b,c,d,e,f,g,h,i,j,k,l,m) = rows |> Seq.head
-                let inferredFrom = rows |> Seq.map(fun (a,b,c,d,e,f,g,h,i,j,k,l,m) -> g) |> Seq.choose id |> Seq.distinct |> String.concat ";"
-                let inferredUsing = rows |> Seq.map(fun (a,b,c,d,e,f,g,h,i,j,k,l,m) -> h) |> Seq.choose id |> Seq.distinct |> String.concat ";"
-                let biodiversityMeasure = rows |> Seq.map(fun (a,b,c,d,e,f,g,h,i,j,k,l,m) -> i) |> Seq.choose id |> Seq.distinct |> String.concat ";"
-                let inferredAs = rows |> Seq.map(fun (a,b,c,d,e,f,g,h,i,j,k,l,m) -> j) |> Seq.distinct |> String.concat ";"
-                (a,b,c,d,e,f,Some inferredFrom, Some inferredUsing, Some biodiversityMeasure, inferredAs, k, l, m)
-            ) |> Seq.toList
-        printfn "Generated %i sites(s)" asSiteGroups.Length
-        let csv = new IndividualMeasureCsv (asSiteGroups |> List.map IndividualMeasureCsv.Row)
+        // 1. Save site-based records
+        let siteRecords = 
+            withSpatialContextAndOutcomes 
+            |> List.map snd
+            |> List.concat
+        printfn "Generated %i sites(s)" siteRecords.Length
+        let csv = new IndividualMeasureCsv (siteRecords |> List.map IndividualMeasureCsv.Row)
         let csvStr = csv.SaveToString('\t')
         System.IO.File.WriteAllText("../thalloo-static-site/map-data/ahbdb_sites.txt", csvStr)
 
